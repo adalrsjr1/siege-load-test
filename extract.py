a9b589bfa6ceddf448b24f19af66cd69575cad44
Adalberto Junior <adalrsjr1@gmail.com>
2017-11-16 09:20:05 -0800

    update scripts

---
+++ extract.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import collections
+import time
+import json
+import subprocess
+import re
+import sys
+
+def outputToDict(output):
+  d = dict()
+
+  for line in output.splitlines():
+    splitted = re.compile(':\s+').split(line)
+    if len(splitted) == 2:
+      d[splitted[0]] = float(splitted[1].split(' ')[0])
+
+  return d
+
+def getValues(l, label):
+  values = list()
+  for d in l:
+     values.append(d[label])
+
+  return values
+
+def runSiege(n, r, server):
+  CONCURRENT = n
+  REPS = r
+  SERVER = server
+  DELAY = 1
+
+  output = subprocess.Popen(['./siege.sh %s %s %s %s'%(CONCURRENT, \
+  REPS, SERVER, DELAY)],\
+  shell=True, stderr=subprocess.PIPE)
+
+  output_str = ''
+  while True:
+    line = output.stderr.readline()
+    if not line:
+       break
+    output_str += line
+  
+  return outputToDict(output_str)
+
+def toJson(d, name):
+  with open(name, 'a') as out:
+    json.dump(d, out)
+    out.write('\n')
+
+def repeat(concurrent, it):
+  for i in range(0, it):
+    d = runSiege(concurrent, 10, '192.168.99.100:30001')
+    d['concurrent'] = concurrent
+    d['iteration'] = i
+    toJson(d, 'evaluation.json')
+
+def main(n, r):
+  for i in range(0,n):
+    repeat(i,r)
+
+main(int(sys.argv[1]), int(sys.argv[2]))
